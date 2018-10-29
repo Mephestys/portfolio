da8b0e0f2f71c795ae987c16f38c7bc42cc1be41
Mephestys <mephestys@gmail.com>
2018-10-29 14:06:27 -0600

    More styling, links work now.

--- src/App.js
+++ src/App.js
@@ -13,22 +13,44 @@ class App extends Component {
           <Navigation />
           <img className="App-banner" src={Banner} alt="" />
           <div className="App-content">
-          <section className='App-section'>
-            <h1>About</h1>
-            <p>I don't have an elevator pitch yet, so here's some random text for your enjoyment.</p>
-          </section>
-          <section className='App-section'>
-            <h1>Projects</h1>
-            <p>Should I be a carousel or a collection of images which are also links, or something else?</p>
-          </section>
-          <section className='App-section'>
-            <h1>Skills</h1>
-            <p>Maybe draw in some neat icons or some crap like that.</p>
-          </section>
-          <section className='App-section'>
-            <h1>Contact</h1>
-            <p>I like the one with the sand.</p>
-          </section>
+            <section className='App-section'>
+              <h1 id="About">About</h1>
+              <div className='App-section-contents'>
+                <p>I don't have an elevator pitch yet, so here's some random text for your enjoyment.</p>
+              </div>
+            </section>
+            <section className='App-section'>
+              <h1 id="Projects">Projects</h1>
+              <div className='App-section-contents'>
+                <p>Should I be a carousel or a collection of images which are also links, or something else?</p>
+              </div>
+            </section>
+            <section className='App-section'>
+              <h1 id="Skills">Skills</h1>
+              <div className='App-skills-wrapper'>
+                <div className='App-skills'>
+                  <div className='App-skills-split'>
+                    <li>HTML/CSS/JS</li>
+                    <li>React</li>
+                    <li>Node.js</li>
+                    <li>Express</li>
+                    <li>Git</li>
+                  </div>
+                  <div className='divider' />
+                  <div className='App-skills-split'>
+                    <li>MongoDB</li>
+                    <li>Django</li>
+                    <li>Python</li>
+                    <li>GraphQL</li>
+                    <li>REST</li>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section className='App-section'>
+              <h1 id="Contact">Contact</h1>
+              <p>I like the one with the sand.</p>
+            </section>
           </div>
         </div>
         <footer>
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -9,14 +9,14 @@ const Navigation = () => {
       <span className="navigation-avatar-name">Daniel Abbott</span>
       <nav>
         <ul className="navigation-internal-links">
-          <li><a href="#">About</a>&lt;</li>
-          <li><a href="#">Projects</a>&lt;</li>
-          <li><a href="#">Skills</a>&lt;</li>
-          <li><a href="#">Contact</a>&lt;</li>
+          <li><a href="#About">About</a>&lt;</li>
+          <li><a href="#Projects">Projects</a>&lt;</li>
+          <li><a href="#Skills">Skills</a>&lt;</li>
+          <li><a href="#Contact">Contact</a>&lt;</li>
         </ul>
         <ul className="navigation-external-links">
-          <li><a href="#">GitHub</a>&lt;</li>
-          <li><a href="#">LinkedIn</a>&lt;</li>
+          <li><a href="https://github.com/Mephestys" rel="external noopener noreferrer" target="_blank">GitHub</a>&lt;</li>
+          <li><a href="https://www.linkedin.com/in/daniel-abbott-19a1a1137/" rel="external noopener noreferrer" target="_blank">LinkedIn</a>&lt;</li>
         </ul>
       </nav>
     </div>
