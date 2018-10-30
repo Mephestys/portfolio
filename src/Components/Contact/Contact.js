f9979c030691e4975932b1f8926e9585d6493492
Daniel Abbott <danielabbott@daniel-abbott.com>
2018-10-30 15:05:17 -0600

    Another shot at Netlify form handling

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,23 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Contact = () => {
-  return (
-    <form name="contact" method="POST" netlify="true">
-      <fieldset>
-        <input name="name" placeholder="Name" type="text" tabIndex="1" required></input>
-      </fieldset>
-      <fieldset>
-        <input name="email" placeholder="Email" type="email" tabIndex="2" required></input>
-      </fieldset>
-      <fieldset>
-        <input name="number" placeholder="Number (optional)" type="tel" tabIndex="3"></input>
-      </fieldset>
-      <fieldset>
-        <textarea name="message" placeholder="Message" tabIndex="4" rows="8" cols="24" required></textarea>
-      </fieldset>
-      <button name="submit" type="submit" data-submit="...Sending">Submit</button>
-    </form>
-  )
+const encode = (data) => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
+
+class Contact extends Component {
+  state= {
+    name: "",
+    email: "",
+    number: "",
+    message: ""
+  }
+
+  handleSubmit = event => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Successfully sent."))
+      .catch(error => alert(error));
+
+    event.preventDefault();
+  }
+
+  handleChange = event => this.setState({ [event.target.name]: event.target.value });
+
+  render () {
+    const { name, email, number, message } = this.state;
+
+    return (
+      <form name="contact" onSubmit={this.handleSubmit}>
+        <fieldset>
+          <input name="name" value={name} placeholder="Name" type="text" onChange={this.handleChange} tabIndex="1" required></input>
+        </fieldset>
+        <fieldset>
+          <input name="email" value={email} placeholder="Email" type="email" onChange={this.handleChange} tabIndex="2" required></input>
+        </fieldset>
+        <fieldset>
+          <input name="number" value={number} placeholder="Number (optional)" type="tel" onChange={this.handleChange} tabIndex="3"></input>
+        </fieldset>
+        <fieldset>
+          <textarea name="message" value={message} placeholder="Message" onChange={this.handleChange} tabIndex="4" rows="8" cols="24" required></textarea>
+        </fieldset>
+        <button name="submit" type="submit">Submit</button>
+      </form>
+    )
+  }
 }
 
 export default Contact;
